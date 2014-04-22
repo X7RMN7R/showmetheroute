9d6f3749bceeae2995cb3bfa16fd48cf3aab9af1
X7RMN7R <experimentaljetsettrashandnostar@hotmail.com>
2014-04-22 15:38:06 +0200

    Test initialisation

---
+++ src/test/java/com/showmetheroute/persistence/domain/fixture/MongoAssertions.java
@@ -0,0 +1,60 @@
+package com.showmetheroute.persistence.domain.fixture;
+
+import com.mongodb.DBObject;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.IndexInfo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MongoAssertions {
+  private MongoOperations ops;
+
+  public static MongoAssertions usingMongo(MongoOperations ops) {
+    MongoAssertions assertions = new MongoAssertions();
+    assertions.ops = ops;
+    return assertions;
+  }
+
+  public CollectionAssertions collections(String name) {
+    return new CollectionAssertions(name);
+  }
+
+  public class CollectionAssertions {
+    private String collection;
+
+    CollectionAssertions(String name) {
+      this.collection = name;
+    }
+
+    public boolean hasIndexOn(String ... fields) {
+      List<IndexInfo> indexes = ops.indexOps(collection).getIndexInfo();
+      for (IndexInfo info : indexes) {
+        if (info.isIndexForFields(Arrays.asList(fields))) {
+	  return true;
+	}
+      }
+      return false;
+    }
+
+    public DocumentAssertions first() {
+      return new DocumentAssertions(ops.getCollection(collection).findOne());
+    }
+
+    public class DocumentAssertions {
+      DBObject document;
+
+      public DocumentAssertions(DBObject document) {
+        this.document = document;
+      }
+
+      public boolean hasField(String name) {
+        return document.containsField(name);
+      }
+
+      public Object fieldContent(String name) {
+        return document.get(name);
+      }
+    }
+  }
+}
