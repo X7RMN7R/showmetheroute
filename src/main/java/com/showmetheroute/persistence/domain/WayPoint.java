7a5f1a6e93c0d99058bdf47317918c765c00f5e9
X7RMN7R <experimentaljetsettrashandnostar@hotmail.com>
2014-04-22 14:15:54 +0200

    Create WayPoint

---
+++ src/main/java/com/showmetheroute/persistence/domain/WayPoint.java
@@ -0,0 +1,22 @@
+package com.showmetheroute.persistence.domain;
+
+public class WayPoint {
+	private double latitude;
+	private double longitude;
+	
+	public double getLatitude() {
+		return this.latitude;
+	}
+	
+	public double getLongitude() {
+		return this.longitude;
+	}
+	
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+}
