c69531732267e5aa43555c95f5d42672e5a383af
X7RMN7R <uv0.xtr@gmail.com>
2014-04-23 21:44:22 +0200

    Track repo tests

---
+++ src/test/java/com/showmetheroute/persistence/integration/TrackRepositoryIntegrationTests.java
@@ -0,0 +1,44 @@
+package com.showmetheroute.persistence.integration;
+
+import WayPointRepository;
+
+import com.showmetheroute.config.MongoConfiguration;
+import com.showmetheroute.persistence.repository.TrackRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static com.showmetheroute.persistence.domain.fixture.PersistenceFixture.standardTrack;
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MongoConfiguration.class})
+public class TrackRepositoryIntegrationTests {
+@Autowired
+  TrackRepository trackRepository;
+
+  @Autowired
+  MongoOperations mongo;
+
+  @Before
+  public void setup() throws Exception {
+    mongo.dropCollection("track");
+  }
+
+  @After
+  public void teardown() {
+    mongo.dropCollection("track");
+  }
+
+  @Test
+  public void thatTrackIsInsertedIntoRepoWorks() throws Exception {
+    assertEquals(0, mongo.getCollection("track").count());
+    trackRepository.save(standardTrack());
+    assertEquals(1, mongo.getCollection("track").count());
+  }
+}
