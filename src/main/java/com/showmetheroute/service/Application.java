3a1f10875e5beb2064a37743095ec512b860918b
X7RMN7R <uv0.xtr@gmail.com>
2014-05-05 20:31:41 +0200

    Using Import annotation to add Mongo configuration to REST application

--- src/main/java/com/showmetheroute/service/Application.java
+++ src/main/java/com/showmetheroute/service/Application.java
@@ -1,32 +1,18 @@
 package com.showmetheroute.service;
 
-import com.mongodb.Mongo;
-import com.showmetheroute.persistence.repository.TrackRepository;
-import org.springframework.context.annotation.Bean;
+import com.showmetheroute.config.MongoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.net.UnknownHostException;
 
-@Configuration
-@EnableMongoRepositories(basePackages = "com.showmetheroute.persistence.repository",
-    includeFilters = @ComponentScan.Filter(value = {TrackRepository.class}, type = FilterType.ASSIGNABLE_TYPE))
+@ComponentScan
+@EnableAutoConfiguration
+@Import(MongoConfiguration.class)
 public class Application {
 
-  public @Bean
-  MongoTemplate mongoTemplate(Mongo mongo) throws UnknownHostException {
-    return new MongoTemplate(mongo, "showmetheroute");
-  }
-
-  public @Bean Mongo mongo() throws UnknownHostException {
-    return new Mongo("localhost");
-  }
-  
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
